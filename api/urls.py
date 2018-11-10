7ba48540f8c0382d47ac6eb325d2953f7b8a60cd
Mickael Souza <mickael.souza.if@gmail.com>
2018-11-10 15:47:38 -0200

    Consumindo api

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from core.views import MarcaViewSet, CarroViewSet
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'carros', CarroViewSet)
@@ -10,5 +11,6 @@ router.register(r'marcas', MarcaViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
+    path('carros', TemplateView.as_view(template_name='index.html')),
 ]
